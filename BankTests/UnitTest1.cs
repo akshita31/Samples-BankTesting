4aaac5af3f41fc7c2bf6cc26483facbba4a3dd20
Akshita Agarwal <akagarw@microsoft.com>
2018-09-19 19:42:42 -0700

    My first commit

---
+++ BankTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using BankAccountNS;
+
+namespace BankTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            //Assert.Equal(expected, actual);
+        }
+    }
+}
